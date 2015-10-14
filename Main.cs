8ce3610bd7c6646e62bf8a83cf58dc32e62afede
NckHmml <NckHmml@users.noreply.github.com>
2015-10-14 18:22:22 +0900

    Added csharp code

---
+++ Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBinaryChallenge
+{
+    public static class Main
+    {
+        public static string ToBinary(int i)
+        {
+            //BEGIN_CHALLENGE
+            return Convert.ToString(i, 2);
+            //END_CHALLENGE
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBinaryChallenge
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestBinary()
+        {
+            Assert.AreEqual("10", Main.ToBinary(2));
+            Assert.AreEqual("1111111", Main.ToBinary(127));
+            Assert.AreEqual("10000100", Main.ToBinary(132));
+        }
+    }
+}
